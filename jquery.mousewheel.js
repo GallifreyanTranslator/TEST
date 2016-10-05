77b16ed0e2dd48e83358a80b192176828285059a
GallifreyanTranslator <rperko28@gmail.com>
2016-10-05 12:13:49 +0200

    Update jquery.mousewheel.js

--- jquery.mousewheel.js
+++ jquery.mousewheel.js
@@ -126,7 +126,7 @@
         // Clearout lowestDelta after sometime to better
         // handle multiple device types that give different
         // a different lowestDelta
-        // Ex: trackpad = 3 and mouse wheel = 120
+        // Ex: trackpad = 4 and mouse wheel = 120
         if (nullLowestDeltaTimeout) { clearTimeout(nullLowestDeltaTimeout); }
         nullLowestDeltaTimeout = setTimeout(nullLowestDelta, 200);
 
